20880bb944a7fc7eada988df0176b147f4a40fcc
Theron Mansilla <theronibarca22@live.com>
2021-10-13 12:12:32 -0400

    Configured flask backend to receive pdf files from front end

--- swarthmore-syllabus-populator/src/Components/HomePage/FileInputBox/FileInputBox.js
+++ swarthmore-syllabus-populator/src/Components/HomePage/FileInputBox/FileInputBox.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const FileInputBox = () => {
-    return (
-        <div>
-            <input type="file" name="file" />
-        </div>
-    )
-}
+  const [file, setFile] = useState(null);
 
-export default FileInputBox
+  return (
+    <div>
+      <form>
+        <input
+          type="file"
+          name="file"
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+          }}
+        />
+        <button
+          onClick={async () => {
+            const data = new FormData();
+            data.append("file", file);
+
+            const response = await fetch("/parse_pdf", {
+              method: "POST",
+              body: data,
+            });
+            if (response.ok) {
+              console.log("request succeeded");
+            } else {
+              console.error("request failed");
+            }
+          }}
+        >
+          Parse Syllabus
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FileInputBox;
--- swarthmore-syllabus-populator/src/Components/HomePage/HomePage.js
+++ swarthmore-syllabus-populator/src/Components/HomePage/HomePage.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
 import FileInputBox from "./FileInputBox/FileInputBox";
 import TextInputBox from "./TextInputBox/TextInputBox";
 import LinkInputBox from "./LinkInputBox/LinkInputBox";
 
 const HomePage = () => {
-    const [inputMethod, setInputMethod] = useState("");
-    return (
-        <div>
-            <div>
-                <button onClick={() => {setInputMethod("File")}}> File </button>
-                <button onClick={() => {setInputMethod("Text")}}> Text </button>
-                <button onClick={() => {setInputMethod("Link")}}> Link </button>
-            </div>
-            {inputMethod === "File" ? <FileInputBox/> : <></>}
-            {inputMethod === "Text" ? <TextInputBox/> : <></>}
-            {inputMethod === "Link" ? <LinkInputBox/> : <></>}
-        </div>
-    )
-}
+  const [inputMethod, setInputMethod] = useState("File");
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => {
+            setInputMethod("File");
+          }}
+        >
+          {" "}
+          File{" "}
+        </button>
+        <button
+          onClick={() => {
+            setInputMethod("Text");
+          }}
+        >
+          {" "}
+          Text{" "}
+        </button>
+        <button
+          onClick={() => {
+            setInputMethod("Link");
+          }}
+        >
+          {" "}
+          Link{" "}
+        </button>
+      </div>
+      {inputMethod === "File" ? <FileInputBox /> : <></>}
+      {inputMethod === "Text" ? <TextInputBox /> : <></>}
+      {inputMethod === "Link" ? <LinkInputBox /> : <></>}
+    </div>
+  );
+};
 
 export default HomePage;
