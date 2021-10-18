413b2c81b2235cc76fa3ae55084065c0cbe386c9
akazako1 <akazako1@swarthmore.edu>
2021-10-18 15:27:54 -0400

    added some text extraction features

--- swarthmore-syllabus-populator/loadPDF-1.py
+++ swarthmore-syllabus-populator/loadPDF-1.py
@@ -5,6 +5,8 @@
 
 import pdfplumber
 import re
+from itertools import product
+
 
 
 # the difference between input() and sys.stdin.readline() is that the former doesn't
@@ -26,9 +28,19 @@ with pdfplumber.open(filename) as pdf:   # open method returns a pdfplumber.PDF 
 
 """
 
-# /Users/arina/Desktop/Swarthmore-Syllabus-Populator/swarthmore-syllabus-populator
+#cd /Users/arina/Desktop/Swarthmore-Syllabus-Populator/swarthmore-syllabus-populator
 
 syll = pdfplumber.open(filename)
+
+if filename.endswith('.pdf'):
+    all_text = '' # new line
+    with pdfplumber.open(filename) as pdf:
+            # page = pdf.pages[0] - comment out or remove line
+            # text = page.extract_text() - comment out or remove line
+            for pdf_page in pdf.pages:
+                single_page_text = pdf_page.extract_text()
+                # separate each page's text with newline
+                all_text = all_text + '\n' + single_page_text
 # first_page = syll.pages[0]
 
 # for line  7th line (ind 6) .replace('old parameter', 'new parameter')
@@ -36,39 +48,72 @@ syll = pdfplumber.open(filename)
 # value = text.split("\n")[6].replace("\t", "").split("R$")[1]
 
 # extract all dates and days of the week
-weekDays = set(["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday", 
-"Mon", "Tue","Wed","Thu","Thur","Fri","Sat","Sun", "MWF", "TTH"])
+
+
+
+keywords = set(("OH", "office", "hours", "meeting", "class", "sessions", 
+"sessions", "drop-in"))
+
+weekDays = set(["monday","tuesday","wednesday","thursday","friday","saturday","sunday", 
+"mon", "tue","wed","thu","thur","fri","sat","sun", "mwt", "tth"])
 
 weekDaysArr = []
+numsArr = []
+foundKeywords = []
 
-for ind, page in enumerate(syll.pages):
-    text = page.extract_text()
-    # Splitting characters in String by ',' OR '_', OR '-', OR ... etc
-    res = re.split(', |_|-|\n| |%|. |\t', text)
-    res =  list(filter(None, res))   #remove empty spaces from the list of words that were parced
-    #print(res) print all words
-    numsArr = []
-    for el in res:
-        if el.isdigit():
-            numsArr.append(el)
-        if el in weekDays:
-            weekDaysArr.append(el)
+numIndsArr = []
+keywordIndsArr = []
+weekdayIndsArr = []
+
+
+#for page_ind, page in enumerate(syll.pages):
+#    text = page.extract_text()
+#    # Splitting characters in String by ',' OR '_', OR '-', OR ... etc
+res = re.split(', |_|-|\n| |%|. |\t', all_text)
+res =  list(filter(None, res))   #remove empty spaces from the list of words that were parced
+
+#print(res) print all words
+for ind, el in enumerate(res):
+    if el.isdigit():
+        numsArr.append(el)
+        numIndsArr.append(ind)
+    elif el.lower() in weekDays:
+        weekDaysArr.append(el)
+        weekdayIndsArr.append(ind)
+    elif el.lower() in keywords:
+        foundKeywords.append(el)
+        keywordIndsArr.append(ind)
+
+
 
         # check if the word is next to other 
 
     
-    print("all nums extracted from page {} are: {}".format(ind, numsArr))
-    print("all week days extracted from page {} are: {}".format(ind, weekDaysArr))
-       
+
+print("{} nums extracted: {}".format(len(numsArr), numsArr))
+print("{} weekdays extracted: {}".format(len(weekDaysArr), weekDaysArr))
+print("{} keywords extracted: {}".format(len(keywordIndsArr), foundKeywords))
 
 
-    # try to identify times by checking if a number is next to 
+print("the closest matching pairs are")
+pairs = sorted(product(numIndsArr, keywordIndsArr), key=lambda t: abs(t[0]-t[1]))
 
 
-#value = text.split("\n")[6].replace("\t", "").split("R$")[1]
-#print("text 1:", value)
-#print("text 2:", )
+#print("print all matched pairs", pairs)
 
-# parce te tables separately 
+for i in range(len(weekDaysArr)):
+    currPair = pairs[i]
+    print(currPair, ":", res[currPair[0]], res[currPair[1]])
+    
+
+
+        # try to identify times by checking if a number is next to 
+
+
+    #value = text.split("\n")[6].replace("\t", "").split("R$")[1]
+    #print("text 1:", value)
+    #print("text 2:", )
+
+    # parce te tables separately 
 
 
