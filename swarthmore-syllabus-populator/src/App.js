54c0680eb5a19c4b53f9e8a6ba5caeb2ef5b1ef1
Shane Jones <shanecjones1999@gmail.com>
2021-10-26 12:30:28 -0400

    added events page in /events

--- swarthmore-syllabus-populator/src/App.js
+++ swarthmore-syllabus-populator/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./Components/HomePage/HomePage"
 import NavigationBar from "./Components/NavigationBar/NavigationBar"
+import Events from './Components/EventsPage/Events'
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <NavigationBar/>
       <Router>
         <Switch>
-          <Route exact path="/" component={HomePage}/> 
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/events" component={Events}/>
         </Switch>
       </Router>
     </div>
---
+++ swarthmore-syllabus-populator/src/Components/EventsPage/Events.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Events = () => {
+    return (
+        <div>
+            Events render here
+        </div>
+    )
+}
+
+export default Events
