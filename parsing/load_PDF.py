df47399c6b0634511f3bad45ba4f5c24255224b7
Theron Imanol Mansilla <66397972+imanolympic@users.noreply.github.com>
2021-11-08 18:15:58 -0500

    reverting back to previous commit

---
+++ parsing/load_PDF.py
@@ -0,0 +1,148 @@
+import os
+import pdfplumber
+from parse_PDF import parse_text_for_events, get_events_list
+import time
+start_time = time.time()
+
+def get_syllabi_directory_path():
+    # change current directory to the parent
+    os.chdir("../")
+
+    return os.path.join(os.getcwd(), "sample-syllabi")
+
+
+def select_syllabi_file(files):
+    choice = ""
+
+    while (not choice.isdigit() or int(choice) < 0 or int(choice) >
+           len(files)):
+        os.system('clear')
+        print("Select a file from sample-syllabi/: ")
+
+        for i, file in enumerate(files):
+            print(f"{i} - {file:<45}")
+
+        choice = input("\nSelect a file: ")
+
+    os.system("clear")
+
+    return files[int(choice)]
+
+
+def extract_syllabi_text(syllabi):
+    all_text = ""
+    with pdfplumber.open(syllabi) as pdf:
+        # page = pdf.pages[0] - comment out or remove line
+        # text = page.extract_text() - comment out or remove line
+        for pdf_page in pdf.pages:
+            single_page_text = pdf_page.extract_text()
+            all_text = all_text + '\n' + single_page_text
+
+    return all_text
+
+
+
+def main():
+    syllabiPath = get_syllabi_directory_path()
+    syllabiFiles = os.listdir(syllabiPath)
+    syllabi = select_syllabi_file(syllabiFiles)
+    text = extract_syllabi_text(os.path.join(syllabiPath, syllabi))
+    # parse_text_for_events(text)
+    get_events_list(text)
+    print("The list of the events extracted from the file:", get_events_list(text))
+
+
+
+main()
+#print("--- %s seconds ---" % (time.time() - start_time))
+
+
+# keywords = ["OH", "office", "hour", "meeting", "class", "sessions", "drop-in"]
+
+
+# weekDays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday",
+#             "mon", "tue", "wed", "thu", "thur", "fri", "sat", "sun", "mwt", "tth"]
+
+# weekDaysArr = []
+# numsArr = []
+# foundKeywords = []
+
+# numIndsArr = []
+# keywordIndsArr = []
+# weekdayIndsArr = []
+
+# # Splitting characters in String by ',' OR '_', OR '-', OR ... etc
+# res = re.split(' |\n', all_text)
+# # remove empty spaces from the list of words that were parced
+# res = list(filter(None, res))
+
+# """
+# for ind, el in enumerate(res):
+#     if el.isdigit():
+#         numsArr.append(el)
+#         numIndsArr.append(ind)
+#     elif el.lower() in weekDays:
+#         weekDaysArr.append(el)
+#         weekdayIndsArr.append(ind)
+#     elif el.lower() in keywords:
+#         foundKeywords.append(el)
+#         keywordIndsArr.append(ind)
+
+
+# print("{} nums extracted: {}".format(len(numsArr), numsArr))
+# print("{} weekdays extracted: {}".format(len(weekDaysArr), weekDaysArr))
+# print("{} keywords extracted: {}".format(len(keywordIndsArr), foundKeywords))
+
+# """
+
+# # ATTEMPT 2: extract the keywords and numbers are the located the closest to each other
+# # in the list of words
+# #print("the closest matching pairs are")
+# #pairs = sorted(product(numIndsArr, keywordIndsArr), key=lambda t: abs(t[0]-t[1]))
+# # for i in range(len(keywordIndsArr)):
+# #    currPair = pairs[i]
+# #    print(currPair, ":", res[currPair[0]], res[currPair[1]])
+
+
+# # ATTEMPT 3: extracting an words before and after
+# # for ind in numIndsArr:
+# #    print("num", res[ind], res[ind-10:ind+10])
+
+# # issues:
+# # PDFs are sometimes formated in 2 columns -- cannot read "across" the line
+
+# # ATTEMPT 5:
+# # new logic:  1) extract all numbers  2) search the substrings for keywords
+
+
+# # create a long string from the result
+# strRes = ''.join(map(str, res))
+# # par.find_all_regex(main_str=strRes, substr="class")  # this will return inds where the word "class" occurs
+# keyword_inds = par.find_all_regex(
+#     main_str=strRes, patterns=keywords)  # returns inds of all keywords
+# num_inds = par.find_all_nums(strRes)
+
+# for k in keyword_inds:  # attempting to match keywords and numbers
+#     # k==index of the last letter of the keyword
+#     print("keyword:", strRes[k[0]:k[1]])
+#     curr_num_inds = par.find_all_nums(strRes[k[1]:k[1]+150])
+#     curr_times_inds = par.find_am_pm(strRes[k[1]:k[1]+150])
+#     curr_weekday_inds = par.find_all_regex(
+#         main_str=strRes[k[1]:k[1]+150], patterns=weekDays)
+#     # adjust the inds
+#     curr_num_inds_s = [x[0]+k[1] for x in curr_num_inds]
+#     curr_num_inds_e = [x[1]+k[1] for x in curr_num_inds]
+#     curr_times_inds = [x+k[1] for x in curr_times_inds]
+#     curr_weekday_inds = [[x[0]+k[1], x[1]+k[1]] for x in curr_weekday_inds]
+
+#     print("matching dates/times")
+#     for s, e in zip(curr_num_inds_s, curr_num_inds_e):
+#         print(strRes[s:e])
+#     for ind in set(curr_times_inds):
+#         print(strRes[ind:ind+2])
+#     for ind in curr_weekday_inds:
+#         print(strRes[ind[0]:ind[1]])
+
+
+# # def is_a_time():
+#     # if there is a am/pm keyword after the number
---
+++ parsing/parse_PDF.py
@@ -0,0 +1,209 @@
+from ast import parse
+import re
+from datetime import *
+
+
+class Event:
+    def __init__(self, time, course_name):
+        self.time = time
+        self.course_name = course_name
+
+    def __str__(self):
+        return f"{self.time[0]}, {self.time[1]}"
+
+    def set_title(self, title):
+        self.title = title
+
+    def set_cathegory(self, cathegory): # Available cathegories: 
+        self.cathegory = cathegory      # "Office Hours", "Class Times", "Help Sessions"
+
+    def set_weekday(self, weekday):
+        self.weekday = weekday
+
+    def cathegorize(self, matches_arr, cathegory):
+        best_dist = float('inf')
+        for match in matches_arr:
+            (ms, me) = match.span()
+            if (ms - me) < best_dist:
+                self.set_title((ms, me))   # (ms, me) are the indices
+                self.set_cathegory(cathegory)
+
+
+def parse_course_name(text):
+    # look for the first number in the doc
+    # the word before that is likely the course title
+    for ind, symbol in enumerate(text):
+         if symbol.isdigit():
+            s = ind
+            ind += 1
+            while ind < len(text) and text[ind].isdigit():
+                ind += 1
+            e = ind    # (s,e) is the first number
+            break 
+    return text[s-6:e]   # TODO: make it more generalizable? 
+ 
+
+
+
+
+def parse_times(text, events):
+    course_name = parse_course_name(text)
+    times_indicies = set()
+    pattern = re.compile(
+        r'\d\d?:?(\d\d)?(am|pm)?(-|\sto\s)\d\d?:?(\d\d)?(am|pm)')
+
+    matches = pattern.finditer(text)
+
+    for match in matches:
+        time_indicie = match.span()
+        if time_indicie not in times_indicies:
+            times_indicies.add(match.span())
+            events.add(Event(time_indicie, course_name))
+
+
+def convert_times(text, event):
+    time = text[event.time[0]:event.time[1]]
+    s = time.lower()
+    if "-" in s:      # check if it's a times range
+        s = s.split("-")
+    else:
+        s = [s]
+    if len(s)==2:
+        if "am" not in s[0] and "pm" not in s[0]:
+            if "am" in s[1]:
+                s[0] += "am"
+            if "pm" in s[1]:
+                s[0] += "pm"
+            
+    for ind, t in enumerate(s):
+        if "pm" in t:
+            t=t.replace("pm"," ")
+            if ":" not in t:
+                t += ":00"
+            t = t.split(":")
+
+            if t[0] != '12':
+                t[0]=str(int(t[0])+12)
+                s[ind] = (":").join(t)
+        elif "am" in t:
+            t = t.replace("am", " ")
+            if ":" not in s:
+                t = t + ":00"
+            t= t.split(":")
+            if t[0] == '12':
+                t[0]='00'
+                s[ind] = (":").join(t)
+    return "-".join(s)
+            
+
+def parse_cathegorize_titles(text, events):
+    oh_pattern = re.compile(
+        r'\b(office hours|oh|)', re.IGNORECASE)
+    class_pattern =  re.compile(
+        r'\b(class|meeting|lecture)', re.IGNORECASE)
+    help_pattern =  re.compile(
+        r'\b(clinic|help|session)', re.IGNORECASE)
+
+    for event in events:
+        s, e = event.time  # get the time associated with the event
+
+        oh_matches = oh_pattern.finditer(text[0:e])
+        class_matches = class_pattern.finditer(text[0:e])
+        help_matches = help_pattern.finditer(text[0:e])
+        
+        event.cathegorize(oh_matches, "Office Hours")
+        event.cathegorize(class_matches, "Class Times")
+        event.cathegorize(help_matches, "Help Sessions")
+
+
+
+
+
+
+def parse_weekdays(text, events):
+    pattern = re.compile(
+        r'\b(monday|mon|tuesday|tues|wednesday|wed|thursday|thurs|friday|fri)',
+        re.IGNORECASE
+    )
+    for event in events:
+        s, e = event.time
+        matches = pattern.finditer(text)
+        best_dist = float('inf')
+        for match in matches:
+            (ms, me) = match.span()
+            if abs(s - me) > 550 or abs(e - ms) > 550:
+                continue
+            if (s - ms) < best_dist:
+                event.set_weekday = text[ms:me]
+        #print("\n")
+
+
+def parse_text_for_events(text):
+    events = set()
+    parse_times(text, events)
+    parse_cathegorize_titles(text, events)
+    parse_weekdays(text, events)
+
+    for event in events:
+        title = event.title
+        #print(text[title[0]:title[1]])
+        #print(convert_times(text, event))  # this will print the time
+        #print("")
+
+
+
+
+
+
+"""
+This function returns a list of event dictionaries, each containing
+1) Event class (course meeting times, OH, help sessions)
+3) Date of the event (if available)
+4) Day of the week (if available)  -- LIST ONLY ONE DAY OF THE WEEK AT A TIME
+5) Time of the event in a 24-hr format (if available)
+"""
+def get_events_dict_list(text):
+    events = set()
+    parse_times(text, events)
+    parse_cathegorize_titles(text, events)
+    parse_weekdays(text, events)
+
+    event_dict_list = []  # a list of events (each event is a dictionary)
+
+    for event in events:
+        event_dict = {}
+        event_dict["course name"] = event.course_name
+        event_dict["event title"] = text[event.title[0]:event.title[1]]
+        event_dict["time"] = convert_times(text, event)
+        event_dict["day of the week"] = text[event.weekday[0]:event.weekday[1]]
+        event_dict["cathegory"] = event.cathegory
+        event_dict_list.append(event_dict)
+    return event_dict_list
+
+
+
+
+
+
+
+"""
+This function returns a list of event objects, with the following fields:
+"""
+def get_events_list(text):
+    events = set()
+    parse_times(text, events)
+    parse_cathegorize_titles(text, events)
+    parse_weekdays(text, events)
+
+    events_list = []  # a list of events (each event is a dictionary)
+
+    for event in events:
+        event.time = convert_times(text, event)
+        events_list.append(event)
+    return events_list
+
+
+
+
+
+
